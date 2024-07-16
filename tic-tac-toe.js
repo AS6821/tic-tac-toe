744242bd7e8449d78cf8f5a78c21159b8ec956b1
AS6821 <abesater@gmail.com>
2024-07-15 20:40:42 -0400

    adds init and render function. removed index.html

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,3 +1,38 @@
-console.log('Hello this is Tic Tac Toe')
+console.log('tic tac toe')
+
+//state
+let turn, board 
 
 
+/*------ GAME LOGIC ------*/
+init()
+
+// purpose of the init function is to put the game in a "starting position"
+function init(){
+    turn = 'X'
+    board = [
+    ' ', ' ', ' ',
+    ' ', ' ', ' ',
+    ' ', ' ', ' '
+    ]
+    render()
+}
+
+function render(){
+
+    // rendering the turn
+    console.log( `It is player ${turn}'s turn`)
+
+    //rendering the board
+    renderBoard()
+}
+
+    function renderBoard(){
+        console.log(' 1 |2| 3');
+        console.log(`1 ${board[0]}|${board[1]}|${board[2]}`);
+        console.log(' --|-|--');
+        console.log(`2 ${board[3]}|${board[4]}|${board[5]}`);
+        console.log(' --|-|--');
+        console.log(`3 ${board[6]}|${board[7]}|${board[8]}`);   
+
+    }
